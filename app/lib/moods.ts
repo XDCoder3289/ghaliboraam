50a80d608803ba2bc466859ff8a1fe08123c9cd8
Salman <xdcoder3289@gmail.com>
2024-05-25 19:55:05 +0500

    homepage v-1

---
+++ app/lib/moods.ts
@@ -0,0 +1,58 @@
+export const moods = [
+  {
+    emoji: "😊",
+    description: "Happy",
+  },
+  {
+    emoji: "😢",
+    description: "Sad",
+  },
+  {
+    emoji: "😰",
+    description: "Stressed",
+  },
+  {
+    emoji: "😠",
+    description: "Angry",
+  },
+  {
+    emoji: "😐",
+    description: "Bored",
+  },
+  {
+    emoji: "😴",
+    description: "Tired",
+  },
+  {
+    emoji: "🤩",
+    description: "Excited",
+  },
+  {
+    emoji: "😔",
+    description: "Lonely",
+  },
+  {
+    emoji: "❤️",
+    description: "Romantic",
+  },
+  {
+    emoji: "🏄‍♂️",
+    description: "Adventurous",
+  },
+  {
+    emoji: "🤒",
+    description: "Sick",
+  },
+  {
+    emoji: "🧐",
+    description: "Focused",
+  },
+  {
+    emoji: "😜",
+    description: "Playful",
+  },
+  {
+    emoji: "😌",
+    description: "Relaxed",
+  },
+];
